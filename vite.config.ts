4776bff9fcde64c2dd17aaa48e552912fb1b6547
Ashwin vemula <ashwin.vemula@cawstudios.com>
2024-12-30 23:15:31 +0530

    touchups

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cricket-real-time-score-board',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
